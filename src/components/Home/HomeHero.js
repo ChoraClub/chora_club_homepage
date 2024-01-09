3e8eb4930380497e4567fac3c0c9c11681b7b942
isha-mistry <ishamistry.lamprostech@gmail.com>
2024-01-09 11:46:45 +0530

    hero component css changes

--- src/components/Home/HomeHero.js
+++ src/components/Home/HomeHero.js
@@ -5,21 +5,21 @@ import rocket from "@/assets/home/rocket.png";
 
 function HomeHero() {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-5 ps-6 pe-4 min-[475px]:ps-10 min-[475px]:pe-6 sm:ps-16 sm:pe-10 py-3 items-center">
+    <div className="grid grid-cols-2 lg:grid-cols-5 ps-6 pe-4 xs:ps-10 xs:pe-6 sm:ps-16 sm:pe-10 py-3 items-center">
       <div className="lg:col-span-3">
-        <div className="font-quanty text-[1.5rem] min-[475px]:text-[1.8rem] sm:text-4xl md:text-[2.5rem] 2xm:text-5xl lg:text-6xl text-light-navy-blue py-3">
+        <div className="font-quanty text-[1.5rem] xs:text-[1.8rem] sm:text-4xl md:text-[2.5rem] 2xm:text-5xl lg:text-6xl text-light-navy-blue py-3">
           Discover<span className="font-poppins">.</span> Learn
           <span className="font-poppins">.</span> Engage
         </div>
-        <div className="pb-4 min-[475px]:pb-7 font-poppins text-xs min-[475px]:text-sm sm:text-base  lg:w-1/2 md:w-2/3">
+        <div className="pb-4 xs:pb-7 font-poppins text-xs xs:text-sm sm:text-base  lg:w-1/2 md:w-2/3">
           A human-centric approach to unlocking Web3 ecosystems!
         </div>
         <div className="font-poppins">
           <button className="flex bg-light-navy-blue rounded-full items-center">
-            <div className="text-white text-xs px-2 py-2 min-[475px]:py-[0.6rem] min-[475px]:text-sm min-[475px]:px-3 sm:px-0 sm:py-4 sm:ps-7 sm:pe-4">
+            <div className="text-white text-xs px-2 py-2 xs:py-[0.6rem] xs:text-sm xs:px-3 sm:px-0 sm:py-4 sm:ps-7 sm:pe-4">
               Start Your Journey
             </div>
-            <div className="border border-white rounded-full p-1 me-[0.3rem] min-[475px]:p-2 min-[475px]:me-[0.4rem] :p-3 sm:me-2">
+            <div className="border border-white rounded-full p-1 me-[0.3rem] xs:p-2 xs:me-[0.4rem] sm:p-3 sm:me-2">
               <Image src={rocket} width={10} alt="rocket"></Image>
             </div>
           </button>
