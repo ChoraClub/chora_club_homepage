069de30f64f5e29ddc6d14c9850e5d83c4d9cb55
purvik6062 <purvikpanchal6336@gmail.com>
2024-03-04 11:30:14 +0530

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import "@rainbow-me/rainbowkit/styles.css";
 import { Providers } from "./providers";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -29,7 +30,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Script>
+        {`
+        <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-56VHGZ3X');</script>
+        `}
+      </Script>
       <body className={`${quanty.variable} ${poppins.variable}`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56VHGZ3X"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         {/* <Providers> */}
         <Navbar />
         {children}
